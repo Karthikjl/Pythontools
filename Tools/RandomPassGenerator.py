d14d7b80b6288513ac2ece8d8ba12796c6be82c5
karthicjl <kar333thic@gmail.com>
2021-07-29 23:04:25 +0530

    Passgen

--- Tools/RandomPassGenerator.py
+++ Tools/RandomPassGenerator.py
@@ -1,15 +1,22 @@
 import random
 
 version = '0.1'
-low = ["karthic","jl","kk"]
-up = ["KK","JL","KARTHIC"]
-numbers = "0123456789"
+low = "abcdefghijklmnopqrstuvwxyz"
+up = "ABCDEFGHIJKLMONPQRSTUVWXYZ"
+# numbers = "0123456789"
 # symbols = "*;/.,_-{}[]()"
 
 
 all = low+up
-length = 3
-
+length = 2
 password = "".join(random.sample(all,length))
 
-print(password)
+
+while True:
+    if password == "Ac":
+        break
+    else:
+        password = "".join(random.sample(all,length))
+        print(password)
+        
+
